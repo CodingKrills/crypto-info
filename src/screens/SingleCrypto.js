470b386dc5fb72bf1b8939d4a04d5e1de7754e18
codingKrills <pulkitsinghmail@gmail.com>
2021-03-16 16:03:05 +0530

    html render in jsx solved in SingleCrypto.js

--- src/screens/SingleCrypto.js
+++ src/screens/SingleCrypto.js
@@ -56,54 +56,58 @@ const SingleCrypto = () => {
               </Spinner>
             </>
           ) : (
-              <div className="row">
-                <div className="col col-12 col-md-8 col-sm-12">
-                  <h1 className="hero-white-h1"> {data.name} </h1>
-                  <p className="coin-info">{data.description.en}</p>
-                </div>
-                <div className="col col-12 col-md-4 col-sm-12">
-                  <img
-                    className="img-fluid"
-                    src={data.image.large}
-                    alt="crypto"
-                  />
-                  <div className="coin-info">
-                    <p>
-                      Official Site:
+            <div className="row">
+              <div className="col col-12 col-md-8 col-sm-12 mt-4">
+                <h1 className="hero-white-h1"> {data.name} </h1>
+                <p
+                  className="coin-info SingleCryptoCard"
+                  style={{ padding: "1.5rem" }}
+                  dangerouslySetInnerHTML={{ __html: data.description.en }}
+                ></p>
+              </div>
+              <div className="col col-12 col-md-4 col-sm-12">
+                <img
+                  className="img-fluid"
+                  src={data.image.large}
+                  alt="crypto"
+                />
+                <div className="coin-info">
+                  <p>
+                    Official Site:
                     <a href={data.links.homepage[0]}>
-                        {data.links.homepage[0]}
-                      </a>
-                    </p>
-                    <p>
-                      Launch Date:{" "}
-                      <span>
-                        {data.genesis_date !== "" ? data.genesis_date : "N/A"}
-                      </span>
-                    </p>
-                    <p>
-                      Country of Origin:{" "}
-                      <span>
-                        {data.country_origin !== "" ? data.country_origin : "N/A"}
-                      </span>
-                    </p>
-                    <p>
-                      Hashing Algorithm:{" "}
-                      <span>
-                        {data.hashing_algorithm !== ""
-                          ? data.hashing_algorithm
-                          : "N/A"}
-                      </span>
-                    </p>
-                    <p>
-                      Change %(7 Days):
+                      {data.links.homepage[0]}
+                    </a>
+                  </p>
+                  <p>
+                    Launch Date:{" "}
+                    <span>
+                      {data.genesis_date !== "" ? data.genesis_date : "N/A"}
+                    </span>
+                  </p>
+                  <p>
+                    Country of Origin:{" "}
+                    <span>
+                      {data.country_origin !== "" ? data.country_origin : "N/A"}
+                    </span>
+                  </p>
+                  <p>
+                    Hashing Algorithm:{" "}
+                    <span>
+                      {data.hashing_algorithm !== ""
+                        ? data.hashing_algorithm
+                        : "N/A"}
+                    </span>
+                  </p>
+                  <p>
+                    Change %(7 Days):
                     <span style={{}}>
-                        {data.market_data.price_change_percentage_7d}
-                      </span>
-                    </p>
-                  </div>
+                      {data.market_data.price_change_percentage_7d}
+                    </span>
+                  </p>
                 </div>
               </div>
-            )}
+            </div>
+          )}
         </div>
       </section>
     </div>
